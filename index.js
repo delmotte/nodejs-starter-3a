826d5538059c84238d96cbd591ae6d1175571c9f
Jean-Yves Delmotte <delmotte.jy@gmail.com>
2018-02-17 10:54:31 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+app.disable('x-powered-by');
+app.use(cors());
+app.use(bodyParser.json({}));
+app.use(morgan('dev'));
+
+// TODO : integrate DB
+// Postgres : https://www.npmjs.com/package/pg
+// mysql : https://www.npmjs.com/package/mysql
+
+app.get('/test', (req, res) => {
+    res.status(200).json({hello: 'world'});
+});
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log('[' + new Date().toISOString() + '] Server launched on port ' + server.address().port + '!');
+});
